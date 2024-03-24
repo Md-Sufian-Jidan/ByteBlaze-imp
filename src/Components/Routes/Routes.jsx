6061abbc6d556d190462ae911bc09fc55d336a41
Md-Sufian-Jidan <jidanjiyaj03@gmail.com>
2024-03-24 10:16:00 +0600

    blogs added

---
+++ src/Components/Routes/Routes.jsx
@@ -0,0 +1,36 @@
+import { createBrowserRouter } from "react-router-dom";
+import MainLayout from "../../../Layouts/MainLayout";
+import Home from "../Pages/Home";
+import Blogs from "../Pages/Blogs";
+import Blog from "../Pages/Blog";
+import BookMarks from "../Pages/BookMarks";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <MainLayout />,
+        children: [
+            {
+                path: '/',
+                element: <Home />
+            },
+
+            {
+                path: '/blogs',
+                element: <Blogs />,
+                loader: () => fetch(`https://dev.to/api/articles?per_page=20&top=7`)
+            },
+            {
+                path: '/blog/:id',
+                element: <Blog />,
+                loader: ({ params }) => fetch(`https://dev.to/api/articles/${params.id}`)
+            },
+            {
+                path: '/bookMarks',
+                element: <BookMarks />
+            }
+        ]
+    },
+]);
+
+export default router
