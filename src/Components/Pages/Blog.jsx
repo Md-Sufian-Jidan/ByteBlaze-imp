626e83e36162793520ef589209494d8af5da6ce7
Md-Sufian-Jidan <jidanjiyaj03@gmail.com>
2024-03-24 08:36:15 +0600

    home page added

--- src/Components/Pages/Blog.jsx
+++ src/Components/Pages/Blog.jsx
@@ -7,7 +7,7 @@ const Blog = () => {
     const { comments_count, title, reading_time_minutes, public_reactions_count, published_at } = blog;
     return (
         <div className="max-w-2xl px-6 py-16 mx-auto space-y-12">
-            <article className="space-y-8 bg-gray-100 text-gray-900">
+            <article className="space-y-8 bg-gray-100 text-gray-900 p-7">
                 <div className="space-y-6">
                     <h1 className="text-4xl font-bold md:tracking-tight md:text-5xl">{title}</h1>
                     <div className="flex flex-col items-start justify-between w-full md:flex-row md:items-center text-gray-600">
@@ -17,13 +17,17 @@ const Blog = () => {
                 </div>
                 {/* tabs */}
                 <div className="flex items-center -mx-4 overflow-x-auto overflow-y-hidden sm:justify-start flex-nowrap bg-gray-100 text-gray-800">
-                    <Link to={''} onClick={()=> setTabIndex(0)} className="flex items-center flex-shrink-0 px-5 py-3 space-x-2 border-b border-gray-600 text-gray-600">
+                    <Link 
+                    // to={''} 
+                    onClick={()=> setTabIndex(0)} className={`flex items-center flex-shrink-0 px-5 py-3 space-x-2 ${tabIndex === 0 ? 'border border-b-0' : 'border-b'} rounded-t-lg border-gray-600 text-gray-900`}>
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="w-4 h-4">
                             <path d="M19 21l-7-5-7 5V5a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2z"></path>
                         </svg>
                         <span>Content</span>
                     </Link>
-                    <Link to={`author`} onClick={()=> setTabIndex(1)} className="flex items-center flex-shrink-0 px-5 py-3 space-x-2 border border-b-0 rounded-t-lg border-gray-600 text-gray-900">
+                    <Link
+                    //  to={`author`}
+                     onClick={()=> setTabIndex(1)} className={`flex items-center flex-shrink-0 px-5 py-3 space-x-2 ${tabIndex === 1 ? 'border border-b-0' : 'border-b'} rounded-t-lg border-gray-600 text-gray-900`}>
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="w-4 h-4">
                             <path d="M2 3h6a4 4 0 0 1 4 4v14a3 3 0 0 0-3-3H2z"></path>
                             <path d="M22 3h-6a4 4 0 0 0-4 4v14a3 3 0 0 1 3-3h7z"></path>
